cd8a404e2193b75ef322d98c1240e5053649d707
maulana-tech <firdaussyah03@gmail.com>
2026-03-14 11:11:28 +0700

    ✏️ update: apps/frontend/astro.config.mjs - update for v1

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,18 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "static",
+  output: "server",
+  adapter: node({ mode: "standalone" }),
   vite: {
-    define: {
-      global: "globalThis",
-    },
-    resolve: {
-      alias: {
-        "@": "/src",
-      },
+    define: { global: "globalThis" },
+    resolve: { alias: { "@": "/src" } },
+    optimizeDeps: {
+      include: ["react", "react-dom", "wagmi", "viem", "recharts"],
     },
   },
 });
